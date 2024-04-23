fbe14b525e2093600c034cd361d648e1b532af33
OGtheZ <edgars.z@reit.lv>
2024-04-23 23:03:46 +0300

    minor visual fix

--- resources/views/blog/update_view.blade.php
+++ resources/views/blog/update_view.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Update your blog') . ' ' . $post->id }}
+            {{ __('Update your blog') . ': ' . $post->title }}
         </h2>
     </x-slot>
 
